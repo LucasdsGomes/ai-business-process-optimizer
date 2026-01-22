6b003326596852759409b86610a4d659ebdcaa4e
Lucas de Souza Gomes <programmingpyrubygo@gmail.com>
2026-01-22 03:14:01 -0300

    refactor: adding a comment for health check

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from app.api.v1.router import api_router
 
+# USO DE HEALTH CHECKS NO PROJETO
+
+# main.py	- App está no ar
+# router.py	- API versionada está acessível
+# endpoints/health.py -	Saúde detalhada de dependências
+
 app = FastAPI(title="AI Business Process Optimizer", version="1.0.0")
 
 app.include_router(api_router)
